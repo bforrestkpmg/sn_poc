0d7a55353a8d4a5d76f5a6cdb4a978e01d25efbd
ben forrest <ben.forrest@gmail.com>
2016-12-02 19:45:46 +1100

    separate console app to test converting doc to html

---
+++ dot_net_convert_doc_files/ConvertDocFiles/ConvertDocFiles/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace ConvertDocFiles
+{
+    class Program
+    {
+
+        public static string GetTempFile(string ext)
+        {
+            string fileName = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ext;
+            return fileName;
+        }
+        //returns extension
+        public static string GetExt(String filename)
+        {
+            return Path.GetExtension(filename);
+        }
+        public static void ConvertDocToHtml(object Sourcepath, object TargetPath)
+        {
+
+            Word._Application newApp = new Word.Application();
+            Word.Documents d = newApp.Documents;
+            object Unknown = Type.Missing;
+            Word.Document od = d.Open(ref Sourcepath, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown, ref Unknown);
+            object format = Word.WdSaveFormat.wdFormatHTML;
+
+            newApp.ActiveDocument.SaveAs(ref TargetPath, ref format,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown);
+
+            newApp.Documents.Close(Word.WdSaveOptions.wdDoNotSaveChanges);
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            String tmpfile = GetTempFile("htm");
+            ConvertDocToHtml(args[1], tmpfile);
+            System.Console.WriteLine("converted: " + tmpfile);
+        }
+    }
+}
