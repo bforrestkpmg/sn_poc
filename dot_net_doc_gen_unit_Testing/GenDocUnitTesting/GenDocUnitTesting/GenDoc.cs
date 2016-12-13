4d1173e50fb20039e9916a3f83524496392e2142
ben forrest <ben.forrest@gmail.com>
2016-12-13 20:48:16 +1100

    initial object with basic extension, write temp file

---
+++ dot_net_doc_gen_unit_Testing/GenDocUnitTesting/GenDocUnitTesting/GenDoc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GenDocUnitTesting
+{
+
+
+    class GenDoc
+    {
+        public string GetTempFile(string ext)
+        {
+            string fileName = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ext;
+            return fileName;
+        }
+        //returns extension
+        public string GetExt(String filename)
+        {
+            return Path.GetExtension(filename);
+        }
+        public static void op_text(String s)
+        {
+            System.Diagnostics.Debug.WriteLine(s);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ dot_net_doc_gen_unit_Testing/GenDocUnitTesting/GenDocUnitTesting/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+
+namespace GenDocUnitTesting
+{
+    [TestClass]
+    public class SupportFunctions
+    {
+        [TestMethod]
+        public void TestGetTempFile()
+        {
+            GenDocUnitTesting.GenDoc g = new GenDocUnitTesting.GenDoc();
+            String res = g.GetTempFile(".blah");
+            Assert.IsNotNull(res, "Get Temp file not returning filename");
+            Assert.IsTrue(res.EndsWith(".blah"), "filename incorrect extension");
+        }
+
+        [TestMethod]
+        public void TestGetExtOK()
+        {
+            GenDocUnitTesting.GenDoc g = new GenDocUnitTesting.GenDoc();
+            String res = g.GetExt("hello.there");
+            Assert.IsNotNull(res, "Get Ext not returning filename");
+            Assert.IsTrue(res.EndsWith(".there"), "filename incorrect extension");
+        }
+
+        [TestMethod]
+        public void TestGetExthandlesblank()
+        {
+            GenDocUnitTesting.GenDoc g = new GenDocUnitTesting.GenDoc();
+            String res = g.GetExt("hello");
+            Assert.IsNotNull(res, "Get Ext not returning filename");
+            Assert.AreEqual(res, "", "filename incorrect extension");
+        }
+
+    } // Test Support Functions
+    [TestClass]
+    public class GenDocUnitTestingOpenFile
+    {
+        [TestMethod]
+        public void ParseArgsOK()
+        {
+        }
+    }
+}
