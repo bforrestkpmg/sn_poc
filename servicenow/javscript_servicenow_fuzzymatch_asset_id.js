bb7af7c040d691a5c07756f60510522dbfac58cd
ben forrest <ben.forrest@gmail.com>
2016-12-15 08:43:33 +1100

    stores distance

--- servicenow/javscript_servicenow_fuzzymatch_asset_id.js
+++ servicenow/javscript_servicenow_fuzzymatch_asset_id.js
@@ -112,9 +112,22 @@ function preparse(s) {
 	return t;
 }
 
+function store_distances(id, dist)
+{
+
+}
+
 function regExpEscape(literal_string) {
     return literal_string.replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, '\\$&');
 }
+
+// stores the match that is the least distance
+// assumes we match the first one if multiple matches
+var top_distance=9999;
+// for current_asset_id the closest
+var closest_match="";
+
+var current_asset_id="";
 
 function iterate_over_detailed_bom(s, id)
 {
@@ -133,6 +146,7 @@ function iterate_over_detailed_bom(s, id)
 	var to_fuzzy_match="";
 	var distance_measure;
 	debug("ID: " + id.toString());
+	current_asset_id=id;
 	for(var i = 0;i < lines.length;i++){
 		theline=lines[i];
 		 reg='(.*)\(' + regExpEscape(id) + '\).*';
@@ -148,16 +162,20 @@ function iterate_over_detailed_bom(s, id)
          // extract () from first
 		matches=first.match(/\(([A-Z][A-Za-z0-9\-]*)\)/)
 	    if (matches === null) { continue; }
-	    debug("fuzzy before: " + matches[1]);
 	    to_fuzzy_match=preparse(matches[1]);
-	    debug("fuzzy after: " + to_fuzzy_match);
-
 	    distance_measure=Levenshtein.get(parsed_id, to_fuzzy_match);
 
+	    if (distance_measure < top_distance)
+	    {
+	    	top_distance=distance_measure;
+	    	closest_match=to_fuzzy_match;
+	    }
 	    arr.push(to_fuzzy_match, distance_measure);
 	    debug("to match with: " + to_fuzzy_match + ", distance: " + distance_measure.toString());
 		 // find asset id within
 	}
+
+	debug("winner is for: " + id.toString() + " is : " + closest_match + " (" + top_distance.toString() + ")" );
 	return arr;
 }
 
@@ -180,7 +198,7 @@ var detail_bom_info;
 
 // simple example for one asset id
 sow_bill_of_materials =  "1.0	WS-4999";
-detail_bom_info = "Firewall-Infrastructure-New-Comple (Cat4999)\t2\t$1,837.33\t$   3,674.66\t$  176,383.68\n Firewall-Support line 1\t2\t$     324.44	$   648.88	$  31,146.24\nFirewall-Support xline2\t2\t$     324.44	$   648.88	$  31,146.24\nFirewall-Infrastructure-New-Complex (ASA5585)\t2\t$    - $   -	$  -\n Firewall-Infrastructure-New-Complex (WS-c4999-F)\t2\t$     1,837.33\t$   3,674.66\t$  176,383.68\n Firewall-Support YYYY\t2\t$     324.44\t$   648.88\t$  31,146.24\n Firewall-Infrastructure-New-Blah (Cat4506)\t$    470.97\t$   1,883.88\t$  90,426.24\n"
+detail_bom_info = "Firewall-Infrastructure-New-Comple (Cat4999)\t2\t$1,837.33\t$   3,674.66\t$  176,383.68\n Firewall-Support line 1\t2\t$     324.44	$   648.88	$  31,146.24\nFirewall-Support xline2\t2\t$     324.44	$   648.88	$  31,146.24\nFirewall-Infrastructure-New-Complex (ASA5585)\t2\t$    - $   -	$  -\n Firewall-Infrastructure-New-Complex (WS-c4998-F)\t2\t$     1,837.33\t$   3,674.66\t$  176,383.68\n Firewall-Support YYYY\t2\t$     324.44\t$   648.88\t$  31,146.24\n Firewall-Infrastructure-New-Blah (Cat4506)\t$    470.97\t$   1,883.88\t$  90,426.24\n"
 // var detail_bom_info="Firewall-Infrastructure-New-Complex (Cat4999)	2	$1,837.33	$   3,674.66	$  176,383.68\n Firewall-Support line 1	2	$     324.44	$   648.88	$  31,146.24\nFirewall-Support xline2	2	$     324.44	$   648.88	$  31,146.24\nFirewall-Infrastructure-New-Complex (ASA5585)	2	$    - $   -	$  -\n Firewall-Infrastructure-New-Complex (WS-c4999-F)	2	$     1,837.33	$   3,674.66	$  176,383.68\n Firewall-Support YYYY	2	$     324.44	$   648.88	$  31,146.24\n Firewall-Infrastructure-New-Blah (Cat4506)	$    470.97	$   1,883.88	$  90,426.24\n"
 
  var distance = Levenshtein.get('Firewall – Infrastructure – New – Complex ', 'Security – Infrastructure –Firewall – New – Complex');   // 2
